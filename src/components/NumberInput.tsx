19a62828b4b3be3faa69dee11bc3240debde57ac
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-15 21:51:13 +0100

    Add NumberInput field and label

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './NumberInput.css';
 
 function NumberInput() {
-  return (
-    <></>
-  );
+	const [idToFilter, setIdToFilter] = useState('');
+
+	return (
+		<div className='number-input'>
+			<form className='number-input-field'>
+				<label htmlFor='filter-by-id'>Filter by id:</label>
+				<input
+					id='filter-by-id'
+					type='text'
+					value={idToFilter}
+					onChange={(event) => setIdToFilter(event.target.value)}
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default NumberInput;
