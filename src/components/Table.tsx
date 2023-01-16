fa0154ee15d757ceed51eda2b51a79bc1c9711a9
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 14:52:25 +0100

    Add initial node creation for the table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,21 @@
+import { useContext } from 'react';
+import { ProductContext } from '../productContext';
 import './Table.css';
 
 function Table() {
-  return (
-    <></>
-  );
+	const productContext = useContext(ProductContext);
+
+	const itemNodes = [];
+
+	if (productContext !== null) {
+		for (const item of productContext.data) {
+			const itemNode = <p>{item.name}</p>;
+
+			itemNodes.push(itemNode);
+		}
+	}
+
+	return <>{itemNodes}</>;
 }
 
 export default Table;
