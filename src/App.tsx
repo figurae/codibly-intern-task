49b0453ff8e1d3388336c5f7e8a83c1335280975
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 14:45:51 +0100

    Fetch data from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import NumberInput from './components/NumberInput';
 import Pagination from './components/Pagination';
 import Table from './components/Table';
+import { ProductContext, apiUrl, ApiInterface } from './productContext';
+import humps from 'humps';
 
 function App() {
-  return (
-    <>
-      <NumberInput />
-      <Table />
-      <Pagination />
-    </>
-  );
+	const [productContext, setProductContext] = useState<ApiInterface | null>(
+		null
+	);
+
+	const fetchFromApiToContext = async () => {
+		const response = await fetch(apiUrl);
+		const jsonData = await response.json();
+		const camelizedJsonData = humps.camelizeKeys(jsonData) as ApiInterface;
+
+		setProductContext(camelizedJsonData);
+	};
+
+	useEffect(() => {
+		fetchFromApiToContext();
+	});
+
+	return (
+		<ProductContext.Provider value={productContext}>
+			<NumberInput />
+			<Table />
+			<Pagination />
+		</ProductContext.Provider>
+	);
 }
 
 export default App;
