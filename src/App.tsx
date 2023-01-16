96463e5b3c83cf32da37db1d1c903fe9a096d914
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 19:40:13 +0100

    Add URLSearchParams

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import Table from './components/Table';
 import { ProductContext, apiUrl, ApiInterface } from './productContext';
 import humps from 'humps';
 
+const ITEMS_PER_PAGE = 5;
+
 function App() {
 	const [productContext, setProductContext] = useState<ApiInterface | null>(
 		null
 	);
 
 	const fetchFromApiToContext = async () => {
-		const response = await fetch(apiUrl);
+		const response = await fetch(
+			apiUrl + new URLSearchParams({ per_page: ITEMS_PER_PAGE.toString() })
+		);
 		const jsonData = await response.json();
 		const camelizedJsonData = humps.camelizeKeys(jsonData) as ApiInterface;
 
@@ -21,7 +25,7 @@ function App() {
 
 	useEffect(() => {
 		fetchFromApiToContext();
-	});
+	}, []);
 
 	return (
 		<ProductContext.Provider value={productContext}>
