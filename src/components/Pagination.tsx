5d740864a023e0818a8c544c7cb12139243eb9ee
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 20:30:07 +0100

    Add preliminary pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,9 +1,25 @@
+import { useContext } from 'react';
+import { ProductContext } from '../productContext';
 import './Pagination.css';
+import MuiPagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
 
 function Pagination() {
-  return (
-    <></>
-  );
+	const productContext = useContext(ProductContext);
+
+	return (
+		<MuiPagination
+			count={productContext?.totalPages}
+			color='primary'
+			renderItem={(item) => (
+				<PaginationItem
+					slots={{ previous: ArrowBack, next: ArrowForward }}
+					{...item}
+				/>
+			)}
+		/>
+	);
 }
 
 export default Pagination;
