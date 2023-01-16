a643a9d0e370225ebb1f8d96319cf1644bef22aa
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 19:40:27 +0100

    Implement the table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,13 +9,30 @@ function Table() {
 
 	if (productContext !== null) {
 		for (const item of productContext.data) {
-			const itemNode = <p>{item.name}</p>;
+			const itemNode = (
+				<tr key={item.id} style={{ backgroundColor: item.color }}>
+					<td>{item.id}</td>
+					<td>{item.name}</td>
+					<td>{item.year}</td>
+				</tr>
+			);
 
 			itemNodes.push(itemNode);
 		}
 	}
 
-	return <>{itemNodes}</>;
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>id</th>
+					<th>name</th>
+					<th>year</th>
+				</tr>
+			</thead>
+			<tbody>{itemNodes}</tbody>
+		</table>
+	);
 }
 
 export default Table;
