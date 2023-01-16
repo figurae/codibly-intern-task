c4e2c0456089d2184aef49fdc31e9148daee70d5
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-01-16 14:45:41 +0100

    Refactor context

---
+++ src/productContext.tsx
@@ -0,0 +1,20 @@
+import { createContext } from 'react';
+
+interface ProductData {
+	id: number;
+	name: string;
+	year: number;
+	color: string;
+	pantoneValue: string;
+}
+
+export interface ApiInterface {
+	data: ProductData[];
+	page: number;
+	perPage: number;
+	total: number;
+	totalPages: number;
+}
+
+export const apiUrl = 'https://reqres.in/api/products';
+export const ProductContext = createContext<ApiInterface | null>(null);
